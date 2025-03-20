0c3701a6bfc468d96125f32c7a5eaef2b6724593
guikominami <contato.mudalab@gmail.com>
2025-03-20 07:12:54 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className='h-screen p-8 flex flex-col items-center justify-center'>
+      <h1 className='text-3xl font-bold'>Hello</h1>
+      <p className='mt-4'>Que merda é essa?</p>
+    </div>
+  );
+}
+
+export default App;
