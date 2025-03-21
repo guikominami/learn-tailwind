6bb5abde9bc2b52cc294bba02955d8f4d04ab6d5
guikominami <contato.mudalab@gmail.com>
2025-03-21 08:57:47 +0100

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import MainPage from './components/MainPage';
 import Card from './components/Card';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      <div className='h-screen bg-orange-500 p-8 flex flex-col items-center justify-center'>
-        <h1 className='text-3xl font-bold'>Hello</h1>
-        <p className='mt-4 mb-4'>O que é tailwind?</p>
-        <Card />
-      </div>
+      <Navbar />
     </>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 import logo from '../assets/icons/logoColor.png';
+import { Link } from 'react-scroll';
 
 const Navbar: React.FC = () => {
   const [nav, setNav] = useState<boolean>(false);
@@ -14,6 +15,26 @@ const Navbar: React.FC = () => {
       <a className='ml-4 max-w-[100px]' href=''>
         <img src={logo} alt='' />
       </a>
+      <ul className='hidden md:flex font-semibold'>
+        <li className='p-5'>
+          <Link to='home' smooth={true} offset={50} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className='p-5'>
+          <Link to='projects' smooth={true} offset={50} duration={500}>
+            Projects
+          </Link>
+        </li>
+        <li className='p-5'>
+          <Link to='about' smooth={true} offset={50} duration={500}>
+            About us
+          </Link>
+        </li>
+      </ul>
+      <div className='block md:hidden'>
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      </div>
     </div>
   );
 };
